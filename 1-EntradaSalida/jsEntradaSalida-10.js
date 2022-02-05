20bba34f860ca9141d0ca82f820632918cd7d8af
thiago <bongioannithiago@gmail.com>
2022-02-04 22:23:59 -0300

    ejercicio

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,14 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let sueldoIngresado;
+	let resultadoSueldo;
+	let reduccionSueldo;
+	
+sueldoIngresado = parseInt(document.getElementById("txtIdImporte").value);
+
+reduccionSueldo = sueldoIngresado * -0.25 ;
+resultadoSueldo = reduccionSueldo + sueldoIngresado;
+
+document.getElementById("txtIdResultado").value = resultadoSueldo;
 }
