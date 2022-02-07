c376e2bd92fbafc196ddf909b52b41ea412c2214
thiago <bongioannithiago@gmail.com>
2022-02-06 22:33:10 -0300

    ejercicios

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -18,3 +18,45 @@ function mostrar()
 //concatenar (+)
     mensajeMostrar = "usted se llama "+nombreIngresado+" y tiene "+edadIngresada+" años";
 	alert(mensajeMostrar);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ignorar ejercicio extra*/
+
+
+
+
+
+/*Se debe mostrar el mensaje : "Perez , usted se llama jose y tiene 66 años" 
+se debe obtener el apellido , de la manera que puedan...*/
+
+//thiago bongioanni 
+//ejercicio E/S 5Bis (ejercicio extra numero 1)
+
+/*function mostrar()
+{   
+//variables declaradas
+let nombreIngresado;
+let edadIngresada;
+let apellidoDelPrompt;
+let mensaje;
+
+//datos
+edadIngresada = document.getElementById("txtIdEdad").value;
+nombreIngresado = document.getElementById("txtIdNombre").value;
+apellidoDelPrompt = prompt("ingrese su apellido");
+
+mensaje ="señor " +apellidoDelPrompt+ ", usted se llama "+nombreIngresado+" y tiene "+edadIngresada+" años";
+alert(mensaje);
+}*/
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,7 +5,7 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	let sueldoIngresado;
+let sueldoIngresado;
 	let resultadoSueldo;
 	let reduccionSueldo;
 	
@@ -15,4 +15,34 @@ reduccionSueldo = sueldoIngresado * -0.25 ;
 resultadoSueldo = reduccionSueldo + sueldoIngresado;
 
 document.getElementById("txtIdResultado").value = resultadoSueldo;
+
 }
+	
+
+
+
+
+
+
+
+
+/*ignorar ejercicio extra*/
+
+
+//E/S 10Bis1: Debemos lograr tomar el importe por ID.T ransformarlo a entero (parseInt),
+ //luego pedirle al usuario un % de descuento (USTEDES RESUELVEN COMO LE PIDEN EL NUMERO)
+ // y mostrar el importe en el cuadro de texto "RESULTADO"	
+
+/*let importeIngresado;
+let totalFinal;
+let descuentoIngresadoPrompt;
+let calculo;
+
+importeIngresado = parseInt(document.getElementById("txtIdImporte").value);
+descuentoIngresadoPrompt = parseInt(prompt("ingrese su descuento"));
+
+calculo = descuentoIngresadoPrompt  * importeIngresado /100;
+
+totalFinal = importeIngresado - calculo;
+
+document.getElementById("txtIdResultado").value = totalFinal;*/
