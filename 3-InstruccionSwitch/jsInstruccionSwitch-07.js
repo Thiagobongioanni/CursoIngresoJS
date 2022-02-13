212a8fe6be417d76b9027eaf712566a879857867
thiago <bongioannithiago@gmail.com>
2022-02-13 01:12:53 -0300

    ejercicios

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,25 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+ let ubicacionElejida;
 
-}//FIN DE LA FUNCIÓN
+     ubicacionElejida = document.getElementById("txtIdDestino").value;
+
+
+     switch(ubicacionElejida){
+           case("Cataratas"):
+                alert("esta ubicado al norte del pais");
+                break;
+
+           case("Mar del plata"):
+                alert("esta ubicado al este del pais");
+                break;
+
+           case("Bariloche"):
+                alert("esta ubicado al oeste del pais");
+                break;
+
+            default:
+                alert("esta ubicado al sur del pais");
+     }
+
+}
