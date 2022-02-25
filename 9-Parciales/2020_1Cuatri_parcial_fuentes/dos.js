a1bfe470034f94d54b137140e83e38bc79c3f710
thiago <bongioannithiago@gmail.com>
2022-02-25 01:46:25 -0300

    en proceso

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,52 @@
 function mostrar()
 {
-  alert("dos");
+ let tipoDeProducto;
+ let cantidadBolsas;
+ let precioIngresado;
+ let descuento;
+ let precioFinal;
+ let respuesta="s";   
+ let mensaje;
+
+     while(respuesta == "s"){
+           tipoDeProducto = prompt("ingrese el producto");
+
+           if(tipoDeProducto != "cal" && tipoDeProducto != "arena" && tipoDeProducto != "cemento" ){
+              tipoDeProducto = prompt("ingrese un producto valido");
+           }
+
+           cantidadBolsas = prompt("ingrese una cantidad de bolsas");
+
+           if(cantidadBolsas < 0){
+              cantidadBolsas = prompt("ingrese una cantidad valida");
+           }
+           
+           precioIngresado = prompt("ingrese el precio");
+
+           if(precioIngresado < 0){
+              precioIngresado = prompt("ingrese un precio valido");
+           }
+
+           if(cantidadBolsas > 10){
+              descuento = 0.15;
+           }
+
+           if(cantidadBolsas > 30){
+              descuento = 0.25;
+           }
+           
+           
+      respuesta = prompt("quiere ingresar mas datos? s/n");    
+     }
+
+     precioFinal = precioIngresado * cantidadBolsas;
+     descuento = precioFinal * descuento;
+     precioFinal = precioFinal - descuento;
+
+     precioBruto = precioIngresado * cantidadBolsas;
+
+     mensaje = "el precio bruto es de "+precioBruto+"\n y el total final con descuento es de "+precioFinal;
+
+     alert(mensaje);
+     
 }
