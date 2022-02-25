ddbcbec8fef417c6b42208449b6aea282e90b80d
thiago <bongioannithiago@gmail.com>
2022-02-25 18:25:00 -0300

    en proceso

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,30 +1,34 @@
 function mostrar()
 {
  let tipoDeProducto;
+ let acumCemento;
+ let precioCemento;
+ let acumCal;
+ let precioCal;
+ let acumArena;
  let cantidadBolsas;
  let precioIngresado;
  let descuento;
  let precioFinal;
- let respuesta="s";   
+ let respuesta="s";
+ let maximoUnidad;   
  let mensaje;
+ let mensaje2;
 
      while(respuesta == "s"){
            tipoDeProducto = prompt("ingrese el producto");
-
-           if(tipoDeProducto != "cal" && tipoDeProducto != "arena" && tipoDeProducto != "cemento" ){
-              tipoDeProducto = prompt("ingrese un producto valido");
+           while(tipoDeProducto != "cal" && tipoDeProducto != "arena" && tipoDeProducto != "cemento" ){
+                 tipoDeProducto = prompt("ingrese un producto valido");
            }
 
            cantidadBolsas = prompt("ingrese una cantidad de bolsas");
-
-           if(cantidadBolsas < 0){
-              cantidadBolsas = prompt("ingrese una cantidad valida");
+           while(cantidadBolsas < 0 || isNaN(cantidadBolsas)== true){
+                 cantidadBolsas = prompt("ingrese una cantidad valida");
            }
            
            precioIngresado = prompt("ingrese el precio");
-
-           if(precioIngresado < 0){
-              precioIngresado = prompt("ingrese un precio valido");
+           while(precioIngresado < 0 || isNaN(precioIngresado)== true){
+                precioIngresado = prompt("ingrese un precio valido");
            }
 
            if(cantidadBolsas > 10){
@@ -34,19 +38,37 @@ function mostrar()
            if(cantidadBolsas > 30){
               descuento = 0.25;
            }
+
+           switch(tipoDeProducto){
+                case "cemento":
+                  acumCemento = acumCemento + cantidadBolsas;
+                  precioCemento = precioIngresado * cantidadBolsas;
+                  break;
+                case "cal":
+                  acumCal = acumCal + cantidadBolsas;
+                  precioCal = precioIngresado * cantidadBolsas;
+                  break;
+                  case "arena":
+           }
            
-           
-      respuesta = prompt("quiere ingresar mas datos? s/n");    
+           respuesta = prompt("quiere ingresar mas datos? s/n");    
      }
+
+     precioBruto = precioIngresado * cantidadBolsas;
+
+     if(descuento != 0){
+        alert("el total bruto es de "+precioBruto);
+     }
+
 
      precioFinal = precioIngresado * cantidadBolsas;
      descuento = precioFinal * descuento;
      precioFinal = precioFinal - descuento;
-
-     precioBruto = precioIngresado * cantidadBolsas;
-
-     mensaje = "el precio bruto es de "+precioBruto+"\n y el total final con descuento es de "+precioFinal;
+     
+     mensaje = " el total final con descuento es de "+precioFinal;
+     mensaje2 = "el producto con mas unidades es "+maximoUnidad
 
      alert(mensaje);
+     alert(mensaje2);
      
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,156 @@
 function mostrar()
+/*{
+let nombreIngresado;
+let edadIngresada;
+let sexoIngresado;
+let estadoCivil;
+let temperaturaCorporal;
+let respuesta="s";
+let bandera=1;
+let mayorTemp;
+let nombreMayTemp;
+let contadorSolterosViudos;
+let contadorMayores;
+let contadorHombresSolteros;
+let acumEdad;
+
+    while(respuesta == "s"){  
+    	  nombreIngresado = prompt("ingrese su nombre");
+
+    	 edadIngresada = prompt("ingrese su edad");
+	     while(isNaN(edadIngresada)== true){
+	           edadIngresada = prompt("ingrese una edad valida");
+	     }
+
+	     sexoIngresado = prompt("ingrese su sexo/genero");
+	     while(sexoIngresado != "f" && sexoIngresado != "m"){
+	           sexoIngresado = prompt("ingrese un sexo valido entre f/m");
+	     }
+
+	     estadoCivil = prompt("ingrese su estado civil");
+	     while(estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil != "viudo"){
+	           estadoCivil = prompt("ingrese un estado civil valido entre: soltero/viudo/casado");
+         }
+
+         temperaturaCorporal = prompt("ingrese su temperatura");
+         while(temperaturaCorporal >= "45" || temperaturaCorporal <= "20"){
+         	   temperaturaCorporal = prompt("ingrese una temperatura valida");
+         }
+
+        respuesta = prompt("quiere ingresar mas datos? s/n");
+        
+        if(bandera == 1 || temperaturaCorporal > mayorTemp){
+           mayorTemp = temperaturaCorporal;
+           nombreMayTemp = nombreIngresado;
+           bandera = 0;
+        }
+
+        if(estadoCivil == "viudo" && edadIngresada > 18){
+           contadorViudos++
+        }
+
+        if(sexoIngresado == "m" && estadoCivil != "casado"){
+           contadorSolterosViudos++
+        } 
+
+        if(edadIngresada > 60 && temperaturaCorporal > 38){
+           contadorMayores++;
+        }
+
+        if(estadoCivil == "soltero" && sexoIngresado == "m"){
+           acumEdad=acumEdad + edadIngresada
+           contadorHombresSolteros++;
+
+        }
+
+    }
+
+     promedio = acumEdad / contadorHombresSolteros; 
+     
+     alert(""+nombreMayTemp);
+     alert(""+contadorViudos);
+     alert(""+contadorSolterosViudos);
+     alert(""+contadorMayores);
+     alert(""+promedio);
+}*/
+
+/*terminarlo{
+let	nombreAlumno;
+let carrera;
+let estadoCarrera;
+let sexoIngresado;
+let edad;
+let nota;
+let contAlumnosP;
+let contAlumnosPsi;
+let contMujeresEnP;
+let contNoBinario;
+let notasFinalizantes;
+let respuesta="s";
+
+
+    while(respuesta == "s"){
+    	 nombreAlumno = prompt("ingrese su nombre");
+
+    	 carrera = prompt("ingrese la carrera");
+         while(carrera != "programacion" && carrera != "psicologia"  && carrera != "diseño grafico"){
+         	   carrera = prompt("ingrese una carrera valida");
+         }
+
+         estadoCarrera = prompt("ingrese el estado de su carrera");
+         while(estadoCarrera != "en curso" && estadoCarrera != "abandono" && estadoCarrera != "finalizado"){
+               estadoCarrera = prompt("ingrese un estado de carrera valido");
+         }
+
+         sexoIngresado = prompt("ingrese su sexo");
+         while(sexoIngresado != "masculino" && sexoIngresado != "femenino" && sexoIngresado != "nobinario"){
+         	   sexoIngresado = prompt("ingrese un sexo valido");
+         }
+
+         edad = prompt("ingrese su edad");
+         while(isNaN(edad)==true){
+         	   edad = prompt("ingrese una edad valida");
+         }
+
+         nota = prompt("ingrese su nota");
+         while(isNaN(nota)==true && nota < 0 && nota >10){
+         	   nota = prompt("ingrese una nota valida");
+         }
+
+         switch(carrera){
+         	    case "programacion":
+         	          contAlumnosP++
+         	          if(sexoIngresado == "femenino"){
+         	          	 contMujeresEnP++
+         	          }
+         	          break;
+
+         	    case "psicologia":
+         	          contAlumnosPsi:
+         	          break;
+
+         	    case "diseño grafico":
+         	          contDG++
+         	          break;
+
+         	    if(sexoIngresado == "nobinario"){
+         	       contNoBinario++;
+                }
+
+                if(){
+                  notasFinalizantes++
+                }
+          }
+
+         respuesta = prompt("quiere ingresar mas datos? s/n");
+    }       
+
+}*/
+
 {
-	alert("tres");
+let ingresoMascota;
+let i;
+
+    for(i=0;i<10;i++)
+    ingresoMascota = prompt("ingrese su mascota");
 }
